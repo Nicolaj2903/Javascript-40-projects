df40169b4d0d6dc573bbf3baac2b62bb5e4dd0d5
Nicolaj2903 <Iversen2903@hotmail.com>
2024-05-07 15:06:26 +0200

    Completed project: 07-questions

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,33 @@
-//using selectors inside the element
-// traversing the dom
+
+// (1. OPTION) using selectors inside the element
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function(question) {
+    // console.log(question);
+    const button = question.querySelector(".question-btn");
+    // console.log(button);
+
+    button.addEventListener('click', function() {
+
+        questions.forEach(function(item) {
+            // console.log(item);
+            if (item !== question) 
+                {
+                    item.classList.remove("show-text");
+                }
+        });
+
+        question.classList.toggle("show-text");
+    });
+});
+
+//(2. OPTION) traversing the dom
+
+// const questionBtns = document.querySelectorAll(".question-btn");
+
+// questionBtns.forEach(function(button) {
+//     button.addEventListener("click", function(event) {
+//         const question = event.currentTarget.parentElement.parentElement;
+//         question.classList.toggle("show-text");
+//     });
+// });
